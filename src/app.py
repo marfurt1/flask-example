bf241e43095c39809e967b9bd1ab86594fb25787
easyundercover <belenfpineyro@gmail.com>
2022-08-05 23:57:12 +0000

    updated

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def hello_flask(): 
     return 'Hello flask'
 
+@app.route('/inicio') #renderiza página web
+def show_home():
+    return render_template('index.html')
+
+@app.route('/url_variables/<string: name>/<int: age>')
+def url_variables(name, age):
+    if age < 18: 
+        return jsonify(message = 'Lo siento' + name + 'no estás autorizado'), 401
+    else:
+        return jsonify(message = 'Bienvenido' + name), 200
+
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
+
