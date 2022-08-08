494e47ba3e04974ddf04ab0b999c0f7decd5aa4f
easyundercover <belenfpineyro@gmail.com>
2022-08-08 22:19:37 +0000

    updated

--- src/app.py
+++ src/app.py
@@ -13,12 +13,18 @@ def hello_flask():
 def show_home():
     return render_template('index.html')
 
-@app.route('/url_variables/<string: name>/<int: age>')
-def url_variables(name, age):
-    if age < 18: 
-        return jsonify(message = 'Lo siento' + name + 'no estás autorizado'), 401
+@app.route('/<string: country>/<string: variety>/<float: aroma>/<float: aftertaste>/<float: acidity>/<float: body>/<float: balance>/<float: moisture>')
+def result(country, variety, aroma, aftertaste, acidity, body, balance, moisture):
+    cols = ['country_of_origin', 'variety', 'aroma', 'aftertaste', 'acidity', 'body', 'balance', 'moisture']
+    data = [country, variety, aroma, aftertaste, acidity, body, balance, moisture]
+    posted = pd.DataFrame(np.array(data).reshape(1,8), columns=cols)
+    loaded_model = pickle.load(open('../models/coffee_model.pkl', 'rb')) #rb es read binary
+    result = loaded_model.predict(posted)
+    text_result = result.tolist()[0]
+    if text_result == 'Yes':
+        return jsonify(message = 'Your coffee is great!'), 200
     else:
-        return jsonify(message = 'Bienvenido' + name), 200
+        return jsonify(message = 'Your coffee sucks!'), 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
